2f0ff1a3e3ceec7e2d75a4bfaabb92be60865bad
이진실 <ljsil1204@naver.com>
2024-03-18 17:32:19 +0900

    feat - tailwind 설정 추가

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -3,14 +3,10 @@ import styled from "styled-components";
 
 const Layout = () => {
   return (
-    <Wrapper>
+    <>
       <Outlet />
-    </Wrapper>
+    </>
   );
 };
-
-const Wrapper = styled.div`
-  background-color: #ddd;
-`;
 
 export default Layout;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+
+const radius_px_0_60 = { ...Array.from(Array(61)).map((_, i) => `${i}px`) };
+const px_4multiple = {
+  ...Array.from(Array(200))
+    .fill(4)
+    .map((v, i) => `${v * i}px`),
+};
+
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-dark": "#0B243A",
+        "primary-light": "#529EE4",
+        primary: "#3481C9",
+      },
+      margin: px_4multiple,
+      padding: px_4multiple,
+      gap: px_4multiple,
+      borderRadius: radius_px_0_60,
+    },
   },
   plugins: [],
 };
