ec02a9c6cd467f1005b2a6574675e9076c81ee4b
이진실 <ljsil1204@naver.com>
2024-03-20 16:37:48 +0900

    feat  - 비주얼 모바일

--- src/components/templates/main/Visual.tsx
+++ src/components/templates/main/Visual.tsx
@@ -22,12 +22,24 @@ const Visual = () => {
         <img
           src="https://onedrive.live.com/embed?resid=C307F97E37B29271%21113&authkey=%21AAit7S3rqkF7-Us&width=1920&height=1000"
           alt=""
+          className="lg:hidden"
+        />
+        <img
+          src="https://onedrive.live.com/embed?resid=C307F97E37B29271%21120&authkey=%21AGYiiK719ObB3iQ&width=1023&height=840"
+          alt=""
+          className="hidden lg:block"
         />
       </SwiperSlide>
       <SwiperSlide>
         <img
           src="https://onedrive.live.com/embed?resid=C307F97E37B29271%21114&authkey=%21ACezNXRukG0s_VM&width=1920&height=1000"
           alt=""
+          className="lg:hidden"
+        />
+        <img
+          src="https://onedrive.live.com/embed?resid=C307F97E37B29271%21119&authkey=%21APyrKw3h2jlbQXw&width=1023&height=840"
+          alt=""
+          className="hidden lg:block"
         />
       </SwiperSlide>
     </SwiperVisual>
