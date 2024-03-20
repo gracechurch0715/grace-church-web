2be3282d8d704e2ed63aba599b1c05b74975746e
이진실 <ljsil1204@naver.com>
2024-03-20 15:31:49 +0900

    feat - 공지사항 페이지 퍼블리싱

--- src/components/pages/Notice.tsx
+++ src/components/pages/Notice.tsx
@@ -1,5 +1,31 @@
+import styled from "styled-components";
+
+import COLORS from "components/common/colors";
+import WRAPPER_WIDTH from "components/common/wrapperWidth";
+import NoticeContent from "components/templates/notice/Content";
+
 const Notice = () => {
-  return <div>Notice</div>;
+  return (
+    <Container>
+      <Title>공지사항</Title>
+      <NoticeContent />
+    </Container>
+  );
 };
+
+const Container = styled.section`
+  max-width: ${WRAPPER_WIDTH}px;
+  margin: 0 auto;
+  padding: 120px 0;
+`;
+
+const Title = styled.h2`
+  margin-bottom: 60px;
+  text-align: center;
+  font-size: 40px;
+  color: ${COLORS.PRIMARY_DARK};
+  letter-spacing: -3px;
+  line-height: 130%;
+`;
 
 export default Notice;
---
+++ src/components/templates/notice/Content.tsx
@@ -0,0 +1,60 @@
+import styled from "styled-components";
+
+import NoticeList from "components/common/NoticeList";
+import COLORS from "components/common/colors";
+
+import notices from "datas/noties";
+
+import arrowLeft from "assets/images/arrow-left.svg";
+import arrowRight from "assets/images/arrow-right.svg";
+
+const arr = [1, 2, 3];
+
+const NoticeContent = () => {
+  return (
+    <>
+      <NoticeList notices={notices} />
+      <div className="flex items-center justify-center gap-5 mt-10">
+        <Btn>
+          <img src={arrowLeft} alt="" />
+        </Btn>
+        <ul className="flex gap-4 items-center">
+          {arr.map((v, i) => (
+            <Page $active={i === 0}>{i + 1}</Page>
+          ))}
+        </ul>
+        <Btn>
+          <img src={arrowRight} alt="" />
+        </Btn>
+      </div>
+    </>
+  );
+};
+
+const Page = styled.li<{ $active: boolean }>`
+  padding: 5px;
+  font-size: 16px;
+  letter-spacing: -2px;
+  line-height: 18px;
+  color: ${({ $active }) => ($active ? COLORS.PRIMARY : COLORS.PRIMARY_DARK)};
+  font-weight: ${({ $active }) => ($active ? 600 : 400)};
+
+  &:hover {
+    text-decoration: ${({ $active }) => ($active ? "none" : "underline")};
+    cursor: pointer;
+  }
+`;
+
+const Btn = styled.button`
+  height: 30px;
+  width: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  img {
+    vertical-align: middle;
+  }
+`;
+
+export default NoticeContent;
